35330cd9e62a05ca5ee3bee939d21f297c57e771
Lorenzo Di Berardino <lorenzo.diberardino03@gmail.com>
2024-04-02 20:45:55 +0200

    Added return code checking in pthread functions

--- Secondo semestre/20240402/Esercizi/es1/src/es1.c
+++ Secondo semestre/20240402/Esercizi/es1/src/es1.c
@@ -12,20 +12,29 @@ typedef struct {
     pthread_mutex_t * mutexes;    
 } thread_data_t;
 
+void errExit(char * msg){
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
 void * printMessage(void *arg){
     thread_data_t * data = (thread_data_t *) arg;
 
     while(data->n > 0){
         data->n--;
         
-        pthread_mutex_lock(&data->mutexes[data->i]);
+        if(pthread_mutex_lock(&data->mutexes[data->i]) != 0)
+            errExit("mutex_lock");
+
         printf("%s", messages[data->i]);
         fflush(stdout);
 
         if(data->i > 0){
-            pthread_mutex_unlock(&data->mutexes[data->i - 1]);
+            if(pthread_mutex_unlock(&data->mutexes[data->i - 1]) != 0)
+                errExit("mutex_unlock");
         } else {
-            pthread_mutex_unlock(&data->mutexes[N - 1]);
+            if(pthread_mutex_unlock(&data->mutexes[N - 1]) != 0)
+                errExit("mutex_unlock");
         }
     }
     
@@ -50,22 +59,27 @@ int main(int argc, char *argv[]){
     pthread_t tids[N];
 
     for(int i = 0; i < N; i++){
-        pthread_mutex_init(&mutexes[i], NULL);
+        if(pthread_mutex_init(&mutexes[i], NULL) != 0)
+            errExit("mutex_init");
+        
         data[i].i = i;
         data[i].mutexes = mutexes;
         data[i].n = n;
 
         if(i < N - 1){
-            pthread_mutex_lock(&mutexes[i]);
+            if(pthread_mutex_lock(&mutexes[i]) != 0)
+                errExit("mutex_lock");
         }
     }
 
     for(int i = 0; i < N; i++){
-        pthread_create(&tids[i], NULL, printMessage, &data[i]);
+        if(pthread_create(&tids[i], NULL, printMessage, &data[i]) != 0)
+            errExit("pthread_create");
     }
 
     for(int i = 0; i < N; i++){
-        pthread_join(tids[i], NULL);
+        if(pthread_join(tids[i], NULL) != 0)
+            errExit("pthread_join");
     }
 
     for(int i = 0; i < N; i++){
--- Secondo semestre/20240402/Esercizi/es2/src/es2.c
+++ Secondo semestre/20240402/Esercizi/es2/src/es2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <errno.h>
 
 #define N 3
 
@@ -9,26 +10,38 @@ typedef struct {
     pthread_mutex_t * mutexes;    
 } thread_data_t;
 
+void errExit(char * msg){
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
 void * printMessage(void *arg){
     thread_data_t * data = (thread_data_t *) arg;
 
-    pthread_mutex_lock(&data->mutexes[data->i]);
+    if(pthread_mutex_lock(&data->mutexes[data->i]) != 0)
+            errExit("mutex_lock");
+
     printf("%c\n", 'A' + data->i);
     fflush(stdout);
 
     if(data->i < N - 1){
-        pthread_mutex_unlock(&data->mutexes[data->i + 1]);
+        if(pthread_mutex_unlock(&data->mutexes[data->i + 1]) != 0)
+            errExit("mutex_unlock");
     } else {
-        pthread_mutex_unlock(&data->mutexes[0]);
+        if(pthread_mutex_unlock(&data->mutexes[0]) != 0)
+            errExit("mutex_unlock");
     }
 
-    pthread_mutex_lock(&data->mutexes[data->i]);
+    if(pthread_mutex_lock(&data->mutexes[data->i]) != 0)
+        errExit("mutex_lock");
+        
     printf("%s (%c)\n", "done", 'A' + data->i);
 
     fflush(stdout);
 
     if(data->i < N - 1){
-        pthread_mutex_unlock(&data->mutexes[data->i + 1]);
+        if(pthread_mutex_unlock(&data->mutexes[data->i + 1]) != 0)
+            errExit("mutex_unlock");
     }
     
     return NULL;
@@ -40,25 +53,30 @@ int main(int argc, char *argv[]){
     pthread_t tids[N];
 
     for(int i = 0; i < N; i++){
-        pthread_mutex_init(&mutexes[i], NULL);
+        if(pthread_mutex_init(&mutexes[i], NULL) != 0)
+            errExit("mutex_init");
+
         data[i].i = i;
         data[i].mutexes = mutexes;
 
         if(i > 0){
-            pthread_mutex_lock(&mutexes[i]);
+            if(pthread_mutex_lock(&mutexes[i]) != 0)
+                errExit("mutex_lock");
         }
     }
 
     for(int i = 0; i < N; i++){
-        pthread_create(&tids[i], NULL, printMessage, &data[i]);
+        if(pthread_create(&tids[i], NULL, printMessage, &data[i]) != 0)
+            errExit("pthread_create");
     }
 
     for(int i = 0; i < N; i++){
-        pthread_join(tids[i], NULL);
+        if(pthread_join(tids[i], NULL) != 0)
+            errExit("pthread_join");
     }
 
     for(int i = 0; i < N; i++){
-        pthread_mutex_destroy(&mutexes[i]);
+        pthread_mutex_destroy(&mutexes[i])
     }
 
     return 0;
